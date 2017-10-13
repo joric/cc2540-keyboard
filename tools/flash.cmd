304bdb60f9e33932c216fc12b52793ba5b9d43d7
joric <joric@users.noreply.github.com>
2017-10-14 01:47:09 +0500

    updated files

--- tools/flash.cmd
+++ tools/flash.cmd
@@ -6,14 +6,14 @@ set ble=%~dp0\..\BLE-CC254x-1.4.0\Projects\ble
 ::set p=%ble%\HIDEmuKbd\CC2540DB\CC2540DK-MINI Keyfob Slave\Exe\HidEmuKbd.hex
 
 ::USB Keyboard
-::set p=%ble%\HIDAdvRemoteDongle\CC2540\CC2540USB AdvRemoteDongle\Exe\HIDAdvRemoteDongle.hex
+set p=%ble%\HIDAdvRemoteDongle\CC2540\CC2540USB AdvRemoteDongle\Exe\HIDAdvRemoteDongle.hex
 
 ::Serial Device
 ::set p=%ble%\HostTestApp\CC2540\hex\HostTestReleaseCC2540Usb.hex
 
 ::Mass Storage Device
 ::set p=%ble%\util\UBL\soc_8051\usb_msd\iar\CC2540-DK\Exe\ubl_usb_msd_8051-DK.hex
-set p=%ble%\util\UBL\soc_8051\usb_msd\iar\CC2540-Nano\Exe\ubl_usb_msd_8051-Nano.hex
+::set p=%ble%\util\UBL\soc_8051\usb_msd\iar\CC2540-Nano\Exe\ubl_usb_msd_8051-Nano.hex
 
 cd /d %~dp0
 
